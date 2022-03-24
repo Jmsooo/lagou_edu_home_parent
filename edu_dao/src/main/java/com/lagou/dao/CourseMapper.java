c3c51258858f76649bc0efa264fde43208960bde
jiaming <jiaming_06@163.com>
2022-03-24 17:29:42 +0800

    图片上传功能

--- edu_dao/src/main/java/com/lagou/dao/CourseMapper.java
+++ edu_dao/src/main/java/com/lagou/dao/CourseMapper.java
@@ -2,6 +2,7 @@ package com.lagou.dao;
 
 import com.lagou.bean.Course;
 import com.lagou.bean.CourseVO;
+import com.lagou.bean.Teacher;
 
 import java.util.List;
 
@@ -13,5 +14,17 @@ public interface CourseMapper {
      * @return
      */
     public List<Course> findCourseByCondition(CourseVO courseVO);
+
+    /**
+     * 新增课程信息
+     * @param course
+     */
+    public void saveCourse(Course course);
+
+    /**
+     * 薪资老师信息
+     * @param teacher
+     */
+    public void saveTeacher(Teacher teacher);
 
 }
--- edu_web/src/main/java/com/lagou/controller/CourseController.java
+++ edu_web/src/main/java/com/lagou/controller/CourseController.java
@@ -7,9 +7,16 @@ import com.lagou.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/course")
@@ -18,11 +25,59 @@ public class CourseController {
     @Autowired
     private CourseService courseService;
 
+    /**
+     * 多条件课程列表查询
+     *
+     * @param courseVO
+     * @return
+     */
     @RequestMapping("/findCourseByCondition")
-    public ResponseResult findCourseByCondition(@RequestBody CourseVO courseVO){
+    public ResponseResult findCourseByCondition(@RequestBody CourseVO courseVO) {
         List<Course> list = courseService.findCourseByCondition(courseVO);
         ResponseResult responseResult = new ResponseResult(true, 200, "响应成功!", list);
         return responseResult;
+    }
+
+    @RequestMapping("/courseUpload")
+    public ResponseResult fileUpload(@RequestParam("file") MultipartFile file, HttpServletRequest request) throws IOException {
+        //1. 判断接收到的上传文件是否为空
+        if (file.isEmpty()) {
+            throw new RuntimeException();
+        }
+
+        //2. 获取项目部署路径
+        //${tomcat}\webapps\edu_web\
+        String realPath = request.getServletContext().getRealPath("/");
+        // /edu-web
+        String contextPath = request.getContextPath();
+        //${tomcat}\webapps\
+        String subString = realPath.substring(0, realPath.indexOf(contextPath.substring(1)));
+
+        //3. 获取原文件名
+        String originalFilename = file.getOriginalFilename();
+
+        //4. 生成新文件名
+        String newFileName = System.currentTimeMillis() + originalFilename.substring(originalFilename.lastIndexOf("."));
+
+        //5. 文件上传
+        String uploadPath = subString + "upload\\";
+        File filePath = new File(uploadPath, newFileName);
+
+        //如果目录不存在,就创建目录
+        if (!filePath.getParentFile().exists()) {
+            filePath.getParentFile().mkdir();
+        }
+
+        //图片上传
+        file.transferTo(filePath);
+
+        //6. 将文件名和文件路径返回,进行响应
+        Map<String, String> map = new HashMap<>();
+        map.put("fileName", newFileName);
+        //http://localhost:8080/upload/
+        map.put("filePath", "http://localhost:8080/upload/" + newFileName);
+
+        return new ResponseResult(true, 200, "图片上传成功!", map);
     }
 
 
